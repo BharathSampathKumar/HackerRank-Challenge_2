f1c0ada936c44a4db77fd87f41ae287b5e3d709a
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2020-07-01 12:10:02 -0700

    fixed merge issue

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -3,29 +3,6 @@ import React from 'react';
 class EmployeesList extends React.Component {
   constructor(props){
     super(props);
-    this.renderMatch = this.renderMatch.bind(this);
-    this.state = {
-      searchResList : this.props.employees
-    }
-  }
-
-  renderMatch(e){
-    let inputTerm = e.target.value;
-    if(inputTerm !== ""){
-      let matches = this.props.employees.filter(employee => employee.name.toLowerCase().includes(inputTerm.toLowerCase()));
-      this.setState({
-        searchResList : matches
-      });
-    }
-    else{
-      this.setState({
-        searchResList : this.props.employees
-      });
-    }
-  }
-
-  constructor(props){
-    super(props);
     this.onFilterInputOnchange = this.onFilterInputOnchange.bind(this);
     this.state = {
       filterTerm : ""
@@ -48,13 +25,11 @@ class EmployeesList extends React.Component {
         <div className="controls">
           <input type="text" className="filter-input" data-testid="filter-input" onChange={this.onFilterInputOnchange}/>
         </div>
-        { resultsList.length > 0 &&
         <ul className="employees-list">
           { employees.filter(employee=>employee.name.toLowerCase().includes(filterTerm.toLowerCase())).map(employee => (
             <li key={employee.name} data-testid="employee">{employee.name}</li>
           ))}
         </ul>
-        }
       </React.Fragment>
     );
   }
