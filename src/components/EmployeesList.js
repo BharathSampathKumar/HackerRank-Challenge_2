dbc4c29e3466a53b9cc2086e24e84092a1c883c4
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2020-07-01 11:08:12 -0700

    Implemented search functions

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
 class EmployeesList extends React.Component {
+  constructor(props){
+    super(props);
+    this.renderMatch = this.renderMatch.bind(this);
+    this.state = {
+      searchResList : this.props.employees
+    }
+  }
+
+  renderMatch(e){
+    let inputTerm = e.target.value;
+    if(inputTerm !== ""){
+      let matches = this.props.employees.filter(employee => employee.name.toLowerCase().includes(inputTerm.toLowerCase()));
+      this.setState({
+        searchResList : matches
+      });
+    }
+    else{
+      this.setState({
+        searchResList : this.props.employees
+      });
+    }
+  }
 
   render() {
-    const { employees } = this.props;
+    const resultsList = this.state.searchResList;
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input type="text" className="filter-input" data-testid="filter-input" onKeyUp={this.renderMatch}/>
         </div>
+        { resultsList.length > 0 &&
         <ul className="employees-list">
-          { employees.map(employee => (
+          { resultsList.map(employee => (
             <li key={employee.name} data-testid="employee">{employee.name}</li>
           ))}
         </ul>
+        }
       </React.Fragment>
     );
   }
