e11073cbb18d94c8fcd49dfd57f1f740b03de5da
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2020-07-01 12:01:07 -0700

    updated render method

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -2,15 +2,32 @@ import React from 'react';
 
 class EmployeesList extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.onFilterInputOnchange = this.onFilterInputOnchange.bind(this);
+    this.state = {
+      filterTerm : ""
+    };
+  }
+
+  onFilterInputOnchange(e){
+    let target = e.target;
+    this.setState({
+      filterTerm : target.value
+    });
+  }
+
   render() {
     const { employees } = this.props;
+    const filterTerm = this.state.filterTerm;
+    
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input type="text" className="filter-input" data-testid="filter-input" onChange={this.onFilterInputOnchange}/>
         </div>
         <ul className="employees-list">
-          { employees.map(employee => (
+          { employees.filter(employee=>employee.name.toLowerCase().includes(filterTerm.toLowerCase())).map(employee => (
             <li key={employee.name} data-testid="employee">{employee.name}</li>
           ))}
         </ul>
